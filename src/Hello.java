0b7cb206772bfda6f0c09c2e92901f61108344c9
albert-magarire <albertmagarire@gmail.com>
2025-08-27 20:49:21 -0400

    New Chnages

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 class Hello {
     public static void main(String a[]){
-        System.out.print(3+5);
+        //declare a variable
+        int number = 3;
+        //print the variable
+        System.out.println(number);
     }
 }
+// This is a simple Java program that declares an integer variable and prints its value to the console.
