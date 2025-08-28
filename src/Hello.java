cb10fd632c0ef7877a4b8d6bcbadd8d66e4cf055
albert-magarire <albertmagarire@gmail.com>
2025-08-28 16:48:23 -0400

    Castiing and Conversions

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,36 @@
 class Hello {
-    public static void main(String a[]){
+    public static void main(String args[]){
         //declare a variable
         int number = 3;
         //print the variable
+        float f = 3.14f;
+        byte b = 22;
+        double d = 33.333;
+        char cd = 'A';
+        boolean bool = true;
+        int num = 100_070_000;
+        System.out.println(num);
+        System.out.println(bool);
+        System.out.println(cd);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(f);
         System.out.println(number);
+
+        char c = 'z';
+        c++;
+        System.out.println(c);
+
+        //byte bt = 127;
+        int somenumber = 257;
+        // It also demonstrates the use of various data types in Java, including float, byte, double, char, and boolean.
+        byte bt = (byte) somenumber; // This is a simple Java program that declares an integer variable and prints its value to the console.
+        // This is a simple Java program that declares an integer variable and prints its value to the console.
+        System.out.println(bt); // Output: 1(because 257 % 256 = 1 )
+
+
+        float f1 = 3.14f;
+        int i1 = (int) f1; // explicit casting from float to int
+        System.out.println(i1); // Output: 3 
     }
 }
-// This is a simple Java program that declares an integer variable and prints its value to the console.
