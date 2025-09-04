e1d824dbd4b39e6e95910151a03324b0fbe602b6
albert-magarire <betserai.magarire@lander.edu>
2025-09-03 23:31:06 -0400

    Scanner and Inputs

--- src/App.java
+++ src/App.java
@@ -1,10 +1,18 @@
-public class App { // Defines a public class named App
+// Specifies the package name
+import java.util.Scanner; // Imports the Scanner class for user input
+public class App {
+     // Defines a public class named App
     public static void main(String[] args) throws Exception { // Main method, entry point of the program
-        double width = 35;
-        double height = 33;
-        double area  = width * height;
-        System.out.println("The area of a rectangle with width "+ width + " and height " + height + " is: " + area); // Prints the area of the rectangle
-
-
+        Scanner word_input = new Scanner(System.in); // Creates a Scanner object to read input from the console
+        System.out.println("Enter a word: ");
+        String user_word = word_input.nextLine();
+        if (user_word.length() < 5){
+            System.out.println("Word is too short, buddy!");
+        }else if ("hello".equals(user_word)){
+            System.out.println("Hi there!");
+        }else{
+            System.out.println("Buddy, your word is: " + user_word);
+        }
+        
     }
 }
